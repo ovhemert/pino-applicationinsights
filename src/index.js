33d398d9581276e5e375d5d67c6f4b4d41c74735
devinrhode2 <devinrhode2@gmail.com>
2022-03-04 15:32:57 -0500

    style: format long `if` statement

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const streams = require('./streams')
 const pumpify = require('pumpify')
 
 async function createWriteStreamSync (options = {}) {
-  if (!options.setup && !options.key && !process.env.APPINSIGHTS_INSTRUMENTATIONKEY) { throw Error('Instrumentation key missing') }
+  if (
+    !options.setup &&
+    !options.key &&
+    !process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+  ) {
+    throw Error('Instrumentation key missing')
+  }
+
   const client = new insights.Client(options)
 
   const parseJsonStream = streams.parseJsonStream()
