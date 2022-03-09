00fb61e171b865cbec0804cbfd9f066eb4591e36
devinrhode2 <devinrhode2@gmail.com>
2022-03-09 07:37:41 -0500

    simply remove async/await/Promise constructs from src no test changes no refactoring no cleanup just remove all async/await stuff

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ const insights = require('./applicationinsights')
 const streams = require('./streams')
 const pumpify = require('pumpify')
 
-async function createWriteStream (options = {}) {
+/**
+ * Should `createWriteStream` need to resolve asynchronously in the future, this method may be removed.
+ */
+function createWriteStreamSync (
+  /**
+   * @type {{
+   *   setup?: (
+   *     appInsights: typeof import('applicationinsights')
+   *   ) => typeof import('applicationinsights').Configuration,
+   *   key?: string
+   * }}
+   */
+  options = {}
+) {
   if (!options.setup && !options.key && !process.env.APPINSIGHTS_INSTRUMENTATIONKEY) { throw Error('Instrumentation key missing') }
   const client = new insights.Client(options)
 
@@ -16,4 +29,6 @@ async function createWriteStream (options = {}) {
   return pumpify(parseJsonStream, batchStream, writeStream)
 }
 
-module.exports.createWriteStream = createWriteStream
+module.exports = {
+  createWriteStreamSync
+}
