4f14a32ab934de84df354f5ad24b3426a647753d
devinrhode2 <devinrhode2@gmail.com>
2022-04-09 08:24:55 -0500

    ignore few remaining type errors

--- src/streams.js
+++ src/streams.js
@@ -1,9 +1,12 @@
 'use strict'
 
+// @ts-expect-error
 const batch2 = require('batch2')
 const split2 = require('split2')
+// @ts-expect-error
 const fastJsonParse = require('fast-json-parse')
 
+// @ts-expect-error
 function batchStream(size) {
   return batch2.obj({
     size,
